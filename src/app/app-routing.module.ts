25dc288ef327f3018599390f11006d7f040ed944
Vadym Kohut <vkohut@sii.pl>
2022-04-20 13:06:55 +0200

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskAddFormComponent } from './task-add-form/task-add-form.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'task-list', component: TaskListComponent },
+  { path: 'add', component: TaskAddFormComponent },
+  { path: '**', redirectTo: 'task-list' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
